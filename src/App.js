373f995972d4b3432c27bb6d552e733c187cb7a8
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2021-06-07 17:43:59 +0300

    Website title and icon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1 class="text">PaintShop</h1>
+            <h4 class="text">Not real paint... Just pixels</h4>
+            <img
+                class="paint-image"
+                alt="PaintPeanutButter"
+                src="https://images.unsplash.com/photo-1605811792180-61ee063690de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+            />
+        </div>
+    );
 }
 
 export default App;
