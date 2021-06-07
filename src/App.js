cea2668054c43c7b84ae1fb1ec7312dce5fcbf6b
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2021-06-07 20:02:08 +0300

    Opening shop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./App.css";
+import products from "./products.js";
 
 function App() {
+    const productList = products.map((product) => {
+        return (
+            <div className="product" key={product.id}>
+                <img
+                    className="product-image"
+                    alt={product.name}
+                    src={product.image}
+                />
+                <p className="caption">
+                    {product.name} <br />
+                    {product.price}KWD
+                </p>
+            </div>
+        );
+    });
+
     return (
         <div>
-            <h1 class="text">PaintShop</h1>
-            <h4 class="text">Not real paint... Just pixels</h4>
-            <img
-                class="paint-image"
-                alt="PaintPeanutButter"
-                src="https://images.unsplash.com/photo-1605811792180-61ee063690de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-            />
+            <div>
+                <h1 className="text">PaintShop</h1>
+                <h4 className="text">Not real paint... Just pixels</h4>
+                <img
+                    id="home-image"
+                    alt="PaintPeanutButter"
+                    src="https://images.unsplash.com/photo-1456086272160-b28b0645b729?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"
+                />
+            </div>
+            <div className="list">{productList}</div>
         </div>
     );
 }
---
+++ src/products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        id: 1,
+        name: "Real Paint",
+        price: 10,
+        image:
+            "https://images.unsplash.com/photo-1585676737728-432f58d5fdba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    },
+    {
+        id:2,
+        name: "Digital Paint",
+        price: 8,
+        image:
+            "https://images.unsplash.com/photo-1563518839049-f44a5e423f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+    },
+];
+
+export default products;
