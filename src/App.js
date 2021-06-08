2c18b1c6886d1319a20afdc3d856e0073b51a738
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2021-06-08 21:43:42 +0300

    yarn add styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import styled from 'styled-components';
 import Home from "./components/Home.js"
 import ProductList from "./components/ProductList.js";
+
 
 function App() {
     return (
