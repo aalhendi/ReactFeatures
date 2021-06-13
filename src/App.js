855c0eecc5bc605ac169b12d1052fd51d9bd006d
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2021-06-13 17:05:48 +0300

    Implemented Theme Button

--- src/App.js
+++ src/App.js
@@ -5,33 +5,41 @@ import { GlobalStyle, ThemeButton } from "./styles";
 import CookieList from "./components/ProductList";
 import Home from "./components/Home";
 import { ThemeProvider } from "styled-components";
+import { useState } from "react";
 
 const theme = {
-  light: {
-    mainColor: "#242424", // main font color
-    backgroundColor: "#fefafb", // main background color
-    pink: "#ff85a2",
-    red: "#ff3232",
-  },
-  dark: {
-    mainColor: "#fefafb", // main font color
-    backgroundColor: "#242424", // main background color
-    pink: "#ff85a2",
-    red: "#ff3232",
-  },
+    light: {
+        mainColor: "#242424", // main font color
+        backgroundColor: "#fefafb", // main background color
+        pink: "#ff85a2",
+        red: "#ff3232",
+    },
+    dark: {
+        mainColor: "#fefafb", // main font color
+        backgroundColor: "#242424", // main background color
+        pink: "#ff85a2",
+        red: "#ff3232",
+    },
 };
 
 function App() {
-  return (
-    <ThemeProvider theme={theme.light}>
-      <GlobalStyle />
-      <ThemeButton onClick={() => alert("I do nothing..")}>
-        Dark Theme
-      </ThemeButton>
-      <Home />
-      <CookieList />
-    </ThemeProvider>
-  );
+    const [currentTheme, setTheme] = useState(theme.light);
+    const toggleTheme = () => {
+        currentTheme === theme.dark
+            ? setTheme(theme.light)
+            : setTheme(theme.dark);
+    };
+    const themeButtonText =
+        currentTheme === theme.dark ? "Light Mode" : "Dark Mode";
+
+    return (
+        <ThemeProvider theme={currentTheme}>
+            <GlobalStyle />
+            <ThemeButton onClick={toggleTheme}>{themeButtonText}</ThemeButton>
+            <Home />
+            <CookieList />
+        </ThemeProvider>
+    );
 }
 
 export default App;
