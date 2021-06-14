8cd0ace478b0fce31a92fb8eac22d7050ec2d983
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2021-06-14 18:52:03 +0300

    First commit

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,15 @@
+import { DetailWrapper } from "../styles";
+
+const ProductDetail = (props) => {
+  return (
+    <DetailWrapper>
+      <h1>{props.product.name}</h1>
+      <img src={props.product.image} alt={props.product.name} />
+      <p>{props.product.description}</p>
+      <p>{props.product.price} KD</p>
+      <button onClick={() => props.setProduct(null)}>Exit</button>
+    </DetailWrapper>
+  );
+};
+
+export default ProductDetail;
---
+++ src/components/ProductList.js
@@ -0,0 +1,31 @@
+// Styling
+import { ListWrapper } from "../styles";
+// Components
+import ProductItem from "./ProductItem";
+import SearchBar from "./SearchBar";
+// Data
+import products from "../products";
+import { useState } from "react";
+
+const ProductList = (props) => {
+    const [query, setQuery] = useState("");
+
+    const productList = products
+        .filter((product) => product.name.includes(query))
+        .map((product) => (
+            <ProductItem
+                product={product}
+                key={product.id}
+                setProduct={props.setProduct}
+            />
+        ));
+
+    return (
+        <>
+            <SearchBar setQuery={setQuery} />
+            <ListWrapper>{productList}</ListWrapper>
+        </>
+    );
+};
+
+export default ProductList;
