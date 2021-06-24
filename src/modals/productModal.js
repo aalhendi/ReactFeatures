d89e167d03d416747f79bb69797a81430c3c5957
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-06-24 16:26:18 +0300

    handleChange method useState

--- src/modals/productModal.js
+++ src/modals/productModal.js
@@ -1,15 +1,72 @@
 //Modal
 import Modal from "react-modal";
+//State & Store
+import { useState } from "react";
 
 const ProductModal = (props) => {
-  //Things
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+    description: "",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    setProduct({ ...product, [e.target.name]: [e.target.value] });
+    console.log(product);
+  };
+
   return (
     <Modal
       isOpen={props.isAddModalOpen}
       onRequestClose={props.closeAddModal}
       contentLabel="Example Modal"
     >
-      Hello
+      <form>
+        <div className="mb-3">
+          <label>Cookie Name</label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Your Favorite Cookie"
+            name="name"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Cookie Price</label>
+          <input
+            className="form-control"
+            type="number"
+            placeholder="20"
+            name="price"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Cookie Description</label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="A Short Description"
+            name="description"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Cookie Image</label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="url"
+            name="image"
+            onChange={handleChange}
+          />
+        </div>
+      </form>
     </Modal>
   );
 };
